addb575f71a58e710d75b2789c49b17fac3ed2d6
Kimberly McCaffrey <Kimberly.M.McCaffrey@gmail.com>
2018-10-09 15:45:18 -0400

    Added Banner component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './index.css';
 import Map from './map.js'
+import Banner from './banner.js'
 
 class App extends Component {
 
   render() {
     return (
       <div className="App">
+        <Banner />
         <Map />
       </div>
     );
---
+++ src/banner.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './index.css';
+
+class Banner extends Component {
+
+  render() {
+    return (
+      <div className="banner">Fareham, UK</div>
+    );
+  }
+}
+
+export default Banner;
