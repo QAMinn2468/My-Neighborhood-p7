e9f18918318ceaa4514a37b6e882b784a5181a7e
Kimberly McCaffrey <Kimberly.M.McCaffrey@gmail.com>
2018-10-10 14:34:28 -0400

    Added details to place Component

--- src/place.js
+++ src/place.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import './index.css';
 
+function( {
+  this function will map the places array iterating over the Place Component
+})
+
 class Place extends Component {
 
   render() {
     return (
-      <div>
+      <div className="place">
+        <h2>{place.title}</h2>
+        <hr />
+        <a className="place-website" href={place.website}>{place.website}</a>
+        <image src={place.photo} alt=`a photo of {place.title}`/>
+        <p className="place-attribution">{place.attribution}</p>
+
       </div>
     );
   }
